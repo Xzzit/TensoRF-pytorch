7a7019ce1b460e6c58c633f57eb1412de1f5ccc2
Xiang Zhizheng <50263991+Xzzit@users.noreply.github.com>
2023-01-20 16:21:57 +0900

    bug fixed, mps device

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ import sys
 # get device
 if torch.cuda.is_available():
     device = torch.device('cuda')
-elif torch.backends.mps.isavailable():
+elif torch.backends.mps.is_available():
     device = torch.device('mps')
 else:
     device = torch.device('cpu')
